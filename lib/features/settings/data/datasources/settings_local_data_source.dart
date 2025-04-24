f471d3071e76b110c99d787d576ad1b2a4d26a9a
edeegg <34895877+edeegg@users.noreply.github.com>
2025-04-24 10:03:35 -0400

    Create settings_local_data_source.dart

---
+++ lib/features/settings/data/datasources/settings_local_data_source.dart
@@ -0,0 +1,26 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import '../../domain/entities/settings_entity.dart';
+
+abstract class SettingsLocalDataSource {
+  Future<SettingsEntity> getSettings();
+  Future<void> saveSettings(SettingsEntity settings);
+}
+
+class SettingsLocalDataSourceImpl implements SettingsLocalDataSource {
+  final SharedPreferences prefs;
+  SettingsLocalDataSourceImpl(this.prefs);
+
+  static const _key = 'app_settings';
+
+  @override
+  Future<SettingsEntity> getSettings() async {
+    final json = prefs.getString(_key);
+    if (json == null) return SettingsEntity(); // defaults
+    return SettingsEntity.fromJson(json);
+  }
+
+  @override
+  Future<void> saveSettings(SettingsEntity settings) async {
+    await prefs.setString(_key, settings.toJson());
+  }
+}
